e52e1b2ba1e0934880636b33047d34a45b34c57b
Euan Steven <23006327@uhi.ac.uk>
2024-07-27 14:36:32 +0000

    UPDATE

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,8 @@
 // Design by Euan Steven
+
+function openAndPrintPDF(pdfUrl) {
+    const newWindow = window.open(pdfUrl, '_blank');
+    newWindow.onload = function() {
+        newWindow.print();
+    };
+}
